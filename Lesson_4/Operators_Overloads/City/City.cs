7f58e86de253a06ccbf74b6c3c41d5de093d4552
Oleksii Dehtiarenko <oleksii.dehtiarenko@namecheap.com>
2024-10-05 19:39:09 +0300

    Add City

---
+++ Lesson_4/Operators_Overloads/City/City.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace City
+{
+    internal class City
+    {
+
+        public string Name { get; set; }
+        public uint Population { get; set; }
+        public string Country { get; set; }
+
+        public City(string name, string country, uint population)
+        {
+            Name = name;
+            Country = country;
+            Population = population;
+        }
+
+        public static City operator +(City city, uint amount)
+        {
+            city.Population += amount;
+            return city;
+        }
+        public static City operator - (City city, uint amount)
+        {
+            city.Population -= amount;
+            return city;
+        }
+        public static bool operator == (City city1, City city2)
+        {
+            return city1.Population == city2.Population;
+        }
+        public static bool operator !=(City city1, City city2)
+        {
+            return city1.Population != city2.Population;
+        }
+        public static bool operator > (City city1, City city2)
+        {
+            return city1.Population > city2.Population;
+        }
+        public static bool operator <(City city1, City city2)
+        {
+            return city1.Population < city2.Population;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is City otherCity)
+            {
+                return this == otherCity;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return Population.GetHashCode();
+        }
+        public override string ToString()
+        {
+            return $"{Name}, {Country}: {Population:N0}";
+        }
+        public void ComparePopulation(City otherCity)
+        {
+            if (this > otherCity)
+            {
+                Console.WriteLine($"{this.Name} has more population than {otherCity.Name}");
+            }
+            else if (this < otherCity)
+            {
+                Console.WriteLine($"{otherCity.Name} has more population than {this.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"{this.Name} and {otherCity.Name} have the same population");
+            }
+        }
+    }
+}
---
+++ Lesson_4/Operators_Overloads/City/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace City
+{
+    #region Task 2
+
+    //Створiть та опишiть клас «Місто».
+    //Виконайте перевантаження
+
+    //+ (для збільшення кількості жителів на вказану кількість),
+    //– (для зменшення кількості жителів на вказану кількість),
+    //== (перевірка на рівність двох міст за кількістю жителів),
+    //< і > (перевірка на меншу чи більшу кількість мешканців),
+    //!= і Equals.
+
+    //Використовуйте механізм властивостей полів класу.
+    #endregion
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            City city1 = new City("Kharkiv", "Ukraine", 2000000);
+            City city2 = new City("Kyiv", "Ukraine", 3000000);
+            City city3 = new City("New York", "Ukraine", 15000000);
+            City city4 = new City("London", "Ukraine", 4000000);
+
+            Console.WriteLine(city1);
+            city1 = city1 + 1500000;
+            Console.WriteLine(city1);
+            city1 = city1 - 1000000;
+
+            city1.ComparePopulation(city2);
+            city1.ComparePopulation(city3);
+            city1.ComparePopulation(city4);
+
+            Console.WriteLine($"Cities {city1.Name} and {city2.Name} have the same population ?: {city1.Equals(city2)}");
+            Console.WriteLine($"Cities {city1.Name} and {city3.Name} have the same population ?: {city1.Equals(city3)}");
+        }
+    }
+}
