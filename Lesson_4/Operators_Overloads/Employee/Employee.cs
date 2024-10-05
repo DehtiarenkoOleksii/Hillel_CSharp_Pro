49b39548d3662fd8fd076c5015ba120a14e89bf0
Oleksii Dehtiarenko <oleksii.dehtiarenko@namecheap.com>
2024-10-05 18:06:23 +0300

    Add Employee

---
+++ Lesson_4/Operators_Overloads/Employee/Employee.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Employee
+    {
+        public int Salary { get; set; }
+        public string Name { get; set; }
+
+        public Employee(int salary, string name)
+        {
+            Salary = salary;
+            Name = name;
+        }
+
+        public static Employee operator + (Employee employee, int amount)
+        {
+            employee.Salary += amount;
+            return employee;
+        }
+        public static Employee operator - (Employee employee, int amount)
+        {
+            employee.Salary -= amount;
+            return employee;
+        }
+
+        public static bool operator == (Employee employee1, Employee employee2) 
+        {
+            return employee1.Salary == employee2.Salary;
+        }
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return employee1.Salary != employee2.Salary;
+        }
+        public static bool operator > (Employee employee1, Employee employee2)
+        {
+            return employee1.Salary > employee2.Salary;
+        }
+        public static bool operator < (Employee employee1, Employee employee2)
+        {
+            return employee1.Salary < employee2.Salary;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is Employee employee_other)
+            {
+                return this == employee_other;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return Salary.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}: {Salary}$";
+        }
+
+        public void CompareSalary(Employee otherEmployee)
+        {
+            if (this > otherEmployee)
+            {
+                Console.WriteLine($"{this.Name} has more salary than {otherEmployee.Name}");
+            }
+            else if (this < otherEmployee)
+            {
+                Console.WriteLine($"{otherEmployee.Name} has more salary than {this.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"{this.Name} and {otherEmployee.Name} have the same salary");
+            }
+        }
+    }
+}
---
+++ Lesson_4/Operators_Overloads/Employee/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Employee
+{
+    #region Task_1
+    //Створiть та опишiть клас «Співробітник».
+    //Додайте до вже створеного класу інформацію про заробітну плату працівника.
+
+    //Виконайте перевантаження
+    //+ (для збільшення зарплати на вказану кількість),
+    //– (для зменшення зарплати на вказану кількість),
+    //== (перевірка на рівність зарплат працівників),
+    //< і > (перевірка на меншу чи більшу кількість зарплат працівників),
+    //!= і Equals.Використовуйте механізм властивостей полів класу.
+    #endregion
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee(6000, "Bob");
+            Employee employee2 = new Employee(6000, "Tom");
+            Employee employee3 = new Employee(7000, "Bob");
+            Employee employee4 = new Employee(5000, "Bob");
+
+            Console.WriteLine(employee1);
+            employee1 = employee1 + 500;
+            Console.WriteLine(employee1);
+            employee1 = employee1 - 500;
+            Console.WriteLine(employee1);
+
+            employee1.CompareSalary(employee3);
+            employee1.CompareSalary(employee4);
+            employee1.CompareSalary(employee2);
+
+            bool areEqual = employee1 == employee2;
+            string result = areEqual ? "yes" : "no";
+            Console.WriteLine($"Employees have the same salary ?: {result}");
+        }
+    }
+}
