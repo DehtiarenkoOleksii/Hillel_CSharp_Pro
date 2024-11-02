8568d1f7c3b6c3404643fa3e891e76eb8e6cc130
Oleksii Dehtiarenko <oleksii.dehtiarenko@namecheap.com>
2024-11-02 16:43:13 +0200

    Add BarberShop

---
+++ Lesson_11/BarberShop/BarberShop.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarberShop
+{
+    using System;
+    using System.Threading;
+
+    internal class BarberShop
+    {
+        private int numberOfChairs;
+        private SemaphoreSlim waitingCustomers; 
+        private SemaphoreSlim barberReady;      
+        private SemaphoreSlim customerReady;    
+        private SemaphoreSlim mutex;            // Mutual exclusion for accessing shared variables
+        private int freeSeats;
+        private bool barberSleeping = false;    // Indicates if the barber is sleeping
+
+        public BarberShop(int chairs)
+        {
+            numberOfChairs = chairs;
+            freeSeats = chairs;
+            waitingCustomers = new SemaphoreSlim(0, chairs);
+            barberReady = new SemaphoreSlim(0, 1);
+            customerReady = new SemaphoreSlim(0, 1);
+            mutex = new SemaphoreSlim(1, 1);
+        }
+
+        public void Barber()
+        {
+            while (true)
+            {
+                // Barber tries to acquire a waiting customer
+                if (waitingCustomers.Wait(1000))
+                {
+                    mutex.Wait();            // Acquire exclusive access
+                    barberSleeping = false;  // Barber wakes up
+                    freeSeats++;             
+                    barberReady.Release();   // Barber is ready to cut hair
+                    mutex.Release();
+                    customerReady.Wait();    
+                    CutHair();
+                }
+                else
+                {
+                    // No customers(( barber goes to sleep
+                    mutex.Wait();
+                    if (!barberSleeping)
+                    {
+                        Console.WriteLine("Barber goes to sleep");
+                        barberSleeping = true;
+                    }
+                    mutex.Release();
+                }
+            }
+        }
+
+        public void Customer(int id)
+        {
+            mutex.Wait(); // Acquire exclusive access
+            if (freeSeats > 0)
+            {
+                freeSeats--;
+                Console.WriteLine($"Customer {id} is waiting. Free seats: {freeSeats}");
+                if (barberSleeping)
+                {
+                    Console.WriteLine($"Customer {id} wakes up the barber");
+                }
+                waitingCustomers.Release(); // Notify the barber of a waiting customer
+                mutex.Release();
+                barberReady.Wait();
+                Console.WriteLine($"Customer {id} is getting a haircut");
+                customerReady.Release(); // Notify the barber that the customer is ready
+            }
+            else
+            {
+                // No free chairs(( leave the barbershop
+                Console.WriteLine($"Customer {id} leaves, no free seats");
+                mutex.Release();
+            }
+        }
+
+        private void CutHair()
+        {
+            Console.WriteLine("Barber is does klac-klac");
+            Thread.Sleep(1000); // Some time for needs for cutting
+            Console.WriteLine("Barber finished cutting hair");
+        }
+    }
+}
---
+++ Lesson_11/BarberShop/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace BarberShop
+{
+    #region Task
+    //Розглянемо перукарню, в якій працює один перукар, є одне крісло для стрижки
+    //і кілька крісел у приймальні для відвідувачів, які чекають на свою чергу.Якщо у перукарні немає відвідувачів,
+    //перукар засинає прямо на своєму робочому місці.Відвідувач, що з'явився, повинен його розбудити,
+    //у результаті перукар приступає до роботи.Якщо в процесі стрижки з'являються нові відвідувачі,
+    //вони повинні або почекати своєї черги, або залишити перукарню, якщо у приймальні немає вільного крісла для очікування.
+    //Завдання полягає в тому, щоб коректно запрограмувати поведінку перукаря та відвідувачів.
+    #endregion
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BarberShop shop = new BarberShop(3); 
+            Thread barberThread = new Thread(new ThreadStart(shop.Barber));
+            barberThread.Start(); 
+
+            Random rand = new Random();
+            int customerId = 1;
+            while (true)
+            {
+                Thread.Sleep(rand.Next(1000, 4000)); // Random  arrival time for new customers
+                int id = customerId++;
+                new Thread(() => shop.Customer(id)).Start(); 
+            }
+        }
+    }
+}
